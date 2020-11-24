79eec26a2869819355bad430d60684f96d44a757
CarlosGG2020 <74953676+CarlosGG2020@users.noreply.github.com>
2020-11-24 09:17:19 +0100

    Esta es la version 3

--- notebooks/CGG_Test/IntGitHub.scala
+++ notebooks/CGG_Test/IntGitHub.scala
@@ -2,6 +2,7 @@
 param=dbutils.widget.get("param")
 print (f"Este es el parametro: "{param})
 print (f"Este es el parametro2: "{param})
+print (f"Este es el parametro3: "{param})
 
 
 // COMMAND ----------
