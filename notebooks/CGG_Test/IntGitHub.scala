e00fca40dbe55fb5444a9179f53ec5facd929ceb
CarlosGG2020 <74953676+CarlosGG2020@users.noreply.github.com>
2020-11-24 10:58:31 +0100

    Esta es la Opcion4

--- notebooks/CGG_Test/IntGitHub.scala
+++ notebooks/CGG_Test/IntGitHub.scala
@@ -9,6 +9,9 @@ print (f"Este es el parametro OPcional: "{param})
 print (f"Este es el segundo parametro OPcional2: "{param})
 print (f"Este es el segundo parametro OPcional3: "{param})
 
+print (f"Este es el segundo parametro OPcional4: "{param})
+
+
 
 // COMMAND ----------
 
