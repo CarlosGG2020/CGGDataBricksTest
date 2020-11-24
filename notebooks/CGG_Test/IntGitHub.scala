7451bd5bc9f4019ba586db67063385892cbdae35
CarlosGG2020 <74953676+CarlosGG2020@users.noreply.github.com>
2020-11-24 11:29:07 +0100

    Update IntGitHub.scala

--- notebooks/CGG_Test/IntGitHub.scala
+++ notebooks/CGG_Test/IntGitHub.scala
@@ -8,6 +8,7 @@ print (f"Este es el segundo parametro OPcional4: "{param})
 print (f"Este es el parametro OPcional: "{param})
 print (f"Este es el segundo parametro OPcional2: "{param})
 print (f"Este es el segundo parametro OPcional3: "{param})
+print (f"Este es el segundo parametro Modificado desde GITHUB: "{param})
 
 
 
